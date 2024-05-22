18c7cd21a0c3bab7470edcb098d51a23dc62646b
Robert Mauro <115938599+rmauro808@users.noreply.github.com>
2024-05-22 15:30:30 -0400

    Create login.py

---
+++ login.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+# Set up Chrome options
+options = Options()
+options.add_experimental_option("detach", True)
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
+# Navigate to practice
+driver.get("http://www.uitestingplayground.com/sampleapp")
+
+time.sleep(1)
+
+#Find the text boxes I am going to input my data
+username = driver.find_element(By.CLASS_NAME, 'form-control')
+password = driver.find_element(By.CSS_SELECTOR, "input[type='password']")
+
+#input a username and the password
+username.send_keys("I am the user!")
+password.send_keys("pwd")
+
+#find the button and click it
+button = driver.find_element(By.CSS_SELECTOR, "button[id='login'")
+button.click()
+
+time.sleep(2)
+
+#Find the logout button and click it
+logout = driver.find_element(By.CSS_SELECTOR, "button[id='login']")
+logout.click()
+
+#pause and close web page.
+time.sleep(2)
+driver.quit()
+
+
+
+
+
